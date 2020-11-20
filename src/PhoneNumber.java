21558847973be85eea085d7bc1bb507864d7bbf8
Tyler <200382154@student.georgianc.on.ca>
2020-11-20 11:23:57 -0500

    Starting the initial Java files, created the phone number first. Moving onto the Customer file now.

---
+++ src/PhoneNumber.java
@@ -0,0 +1,52 @@
+public class PhoneNumber {
+    private String areaCode, cityCode, lineNumber;
+
+    public PhoneNumber(String areaCode, String cityCode, String lineNumber) {
+        setAreaCode(areaCode);
+        setCityCode(cityCode);
+        setLineNumber(lineNumber);
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        if(areaCode.matches("\\(?[2-9]\\d{2}")) {
+            this.areaCode = areaCode;
+        }
+        else {
+            throw new IllegalArgumentException("Area code must be 3 digits, the first digit must be 2-9, digits 2 and three can be any valid number");
+        }
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        if(cityCode.matches("\\(?[2-9]\\d{2}")) {
+            this.cityCode = cityCode;
+        }
+        else {
+            throw new IllegalArgumentException("City code must be 3 digits, the first digit must be 2-9, digits 2 and three can be any valid number");
+        }
+    }
+
+    public String getLineNumber() {
+        return lineNumber;
+    }
+
+    public void setLineNumber(String lineNumber) {
+        if (lineNumber.matches("d{4}")) {
+            this.lineNumber = lineNumber;
+        } else {
+            throw new IllegalArgumentException("Line number must be 4 digits.");
+        }
+    }
+
+    public String toString()
+        { return String.format("Phone number is: %s-%s %s",
+                    areaCode, cityCode, lineNumber);
+        }
+}
---
+++ src/TestNonGUI.java
@@ -0,0 +1,38 @@
+//Tyler Lindley 200382154
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class TestNonGUI {
+    public static void main(String[] args) throws SQLException {
+
+//        System.out.println("DataUtility class - challenge 1");
+//        ArrayList<Customer> customers = DataUtility.getCustomers();
+//        System.out.println("this should equal 1000: " + customers.size());
+//
+//        System.out.println("\nDataUtility class - challenge 2");
+//        ArrayList<Customer> tallest = DataUtility.getTallest(customers);
+//        System.out.println("This should display the 8 tallest customers. " +
+//                "ID's 66, 160, 222, 242, 445, 516, 549, 698");
+//        tallest.stream().forEach(System.out::println);
+//
+//        System.out.println("\nDataUtility class - challenge 3");
+//        ArrayList<Customer> areaCodeList = DataUtility.getCustomersInAreaCode(customers, "416");
+//        System.out.println("This should display 125 records: "+areaCodeList.size());
+//
+//        System.out.println("\nDataUtility class - challenge 4");
+//        ArrayList<Customer> searchListResults = DataUtility.getCustomersWithString(customers, "Rose");
+//        System.out.println("This should display 3 records: "+searchListResults.size());
+//        searchListResults.stream().forEach(System.out::println);
+//
+//        System.out.println("\nDataUtility class - challenge 5");
+//        double avgBMI = DataUtility.getAverageBMI(customers);
+//        System.out.printf("This should return 28.2. Returned: %.1f: %n",avgBMI);
+//
+//        System.out.println("\nDataUtility class - challenge 6");
+//        TreeMap<String, Integer> bloodTypeCounts = DataUtility.getBloodTypeCounts(customers);
+//        for (String bloodType: bloodTypeCounts.keySet())
+//            System.out.printf("blood type: %-3s  count: %3d%n",bloodType, bloodTypeCounts.get(bloodType));
+//
+    }
+}
